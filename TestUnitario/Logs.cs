a9620ade77afea3aaec52b1f69b7d1417b072df4
Gri2019 <54857324+Gri2019@users.noreply.github.com>
2019-09-04 17:42:52 -0300

    Add files via upload

---
+++ TestUnitario/Logs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestUnitario
+{
+    [TestClass]
+    public class Logs
+    {
+        [TestMethod]
+        public void ResultadoEsperado()
+        {
+
+            // Definición de parámetros para la prueba
+            bool consola = true;
+            bool archivo = false;
+            bool bDatos = false;
+            bool esperado = false;
+            
+
+            // Llamado de método de clase
+            bool rdo = Logeo.JobbLogger.ControlarInicio(consola, archivo, bDatos);
+
+            // Controlar que la prueba cumpla con el valor esperado
+            Assert.AreEqual(esperado, rdo);
+
+        }
+
+        [TestMethod]
+        public void ResultadoNoEsperado()
+        {
+
+            Assert.ThrowsException<System.Exception>(() => Logeo.JobbLogger.ControlarInicio(false, false, false));
+
+
+        }
+    }
+}
